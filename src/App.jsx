ab63ddc5d84c103afee4aa0212b087028190a3aa
Lini Lorance <lini.lorance@codinghands.in>
2025-08-06 02:34:04 +0530

    Add Prettierr  configuration, update HTML doctype, enhance package.json with linting and formatting scripts, and set up Husky for pre-commit and pre-push hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import "./App.css";
+import './App.css'
 
 function App() {
-  return (
-    <>
-      <div>App</div>
-    </>
-  );
+    return (
+        <>
+            <div>App</div>
+        </>
+    )
 }
 
-export default App;
+export default App
